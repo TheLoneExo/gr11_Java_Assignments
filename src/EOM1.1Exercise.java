d754ac85e3a85a0784830fa87d8f568d413a6b95
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-04 23:38:32 -0500

    1.1Exercise

--- src/EOM1.1Exercise.java
+++ src/EOM1.1Exercise.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bonjour Monde!");
+        System.out.println("2 + 2 is " + (2 + 2) + " - 1 that's " + (4-1) + " quick maths" );
+        System.out.println("Also, the remainder of 7 divided by 2 is " + (5%2));
+        //Good committing
     }
 }
 
