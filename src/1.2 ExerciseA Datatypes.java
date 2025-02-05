f1de842c0cf0b7341967b53eace9e1f27a621a7e
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-05 15:06:52 -0500

    Commit

---
+++ src/1.2 ExerciseA Datatypes.java
@@ -0,0 +1,41 @@
+//Variables
+class class1_2_Datatypes {
+    public static void main(String[] args) {
+
+        /*  ~~~~~ Lesson ~~~~~ */
+        int age = 16; //declares (makes) a variable called age and initializes the value of 16 to age
+        System.out.print("Your age is: "); //Prints the message in the brackets
+        System.out.print(age); //Prints the value 16
+
+        //The lines above print on one line. To print on different lines use:
+        System.out.println(); // This goes to the next line. Note: this is letter l not #1.
+        System.out.println("Your age is: "); //Prints the message on a New Line
+        System.out.println(age); //Prints the value 16 on a New Line
+
+        //We can make lots of different types of variables. Examples:
+        double height = 1.63; //metres.  Doubles and floats are decimals
+        boolean isPresent = true; //recall booleans are true or false
+        char firstLetter = 'S'; //a char is a single ASCII character inside single quotes
+
+        /*  ~~~~~ Practice ~~~~~ */
+
+        //Question 1
+        //a) Declare a variable for a name and initialize to Joe Smith.
+
+        //b) Print the variable on a new line.
+
+
+        //Question 2
+        //a) Declare a variable for birthyear, and initialize it to 2004.
+
+        //b) Print the variable on a new line.
+
+
+        //Question 3
+        //a) Declare a variable for weight and initialize it to 45.6 (kg).
+
+        //b) Print the variable on a new line.
+
+
+    }
+}
---
+++ src/1.2 ExerciseB Keyboard Input.java
@@ -0,0 +1,45 @@
+//Keyboard Input
+
+import java.util.Scanner; //we need this line to be able to get input from the keyboard
+
+class Exercise1_2_Keyboard_Input {
+    public static void main(String[] args) {
+        /* import java.util.Scanner;
+
+class Exercise1_2_KB_Input {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in); //#2
+        System.out.println("What is your name?");
+        String name = console.nextLine(); //#3 input
+        System.out.println("Hi "+name);
+
+        System.out.println("Number?");
+        int num = console.nextInt();
+        System.out.println(num*10 + " is 10 times better");
+
+    }
+}
+ ~~~~~ Lesson ~~~~~ */
+        Scanner console = new Scanner(System.in); //makes a Scanner called console to listen to keyboard input
+        System.out.println("Write your name and press enter:"); //prints instructions to the user
+        String name = console.nextLine(); //saves the user's keyboard input to a variable message
+        System.out.print("Welcome, ");
+        System.out.println(name);
+
+        //You can also input integers and doubles
+        System.out.println("How old are you?");
+        int age = console.nextInt(); //declare an int variable, get the user input and assign to the age
+        System.out.print(name);
+        System.out.print(" is age ");
+        System.out.println(age);
+
+        console.nextLine(); //after using nextInt() or nextDouble(), write this line to clear the value and prepare for the next String
+
+
+        /*  ~~~~~ Practice ~~~~~ */
+        //Question 1: Ask the user for their favourite animal, save it to a variable, and print: Your favourite animal is <their animal>
+
+
+        //Question 2: Ask the user to answer this skill testing question: "What is 3 + 10?"  (use an int)
+    }
+}
