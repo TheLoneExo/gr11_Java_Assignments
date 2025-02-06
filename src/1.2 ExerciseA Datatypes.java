8f7d3399987a7f7571687e85115aefb542f3e2a3
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-06 14:00:36 -0500

    Commit

--- src/1.2 ExerciseA Datatypes.java
+++ src/1.2 ExerciseA Datatypes.java
@@ -1,5 +1,5 @@
 //Variables
-class class1_2_Datatypes {
+class Exercise1_2_Datatypes {
     public static void main(String[] args) {
 
         /*  ~~~~~ Lesson ~~~~~ */
@@ -13,29 +13,25 @@ class class1_2_Datatypes {
         System.out.println(age); //Prints the value 16 on a New Line
 
         //We can make lots of different types of variables. Examples:
-        double height = 1.63; //metres.  Doubles and floats are decimals
-        boolean isPresent = true; //recall booleans are true or false
-        char firstLetter = 'S'; //a char is a single ASCII character inside single quotes
-
         /*  ~~~~~ Practice ~~~~~ */
 
         //Question 1
         //a) Declare a variable for a name and initialize to Joe Smith.
-
+        String name = "Joe Smith";
         //b) Print the variable on a new line.
-
+        System.out.println(name);
 
         //Question 2
         //a) Declare a variable for birthyear, and initialize it to 2004.
-
+        int birthyear = 2004;
         //b) Print the variable on a new line.
-
+        System.out.println(birthyear);
 
         //Question 3
         //a) Declare a variable for weight and initialize it to 45.6 (kg).
-
+        double weight = 45.6;
         //b) Print the variable on a new line.
-
+        System.out.println(weight);
 
     }
 }
