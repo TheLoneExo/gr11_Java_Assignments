dc47c77c3b1f439330b1e8f88a91adbd36b27c71
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-10 15:07:37 -0500

    Commit

---
+++ src/Exercise1.4.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class Exercise1_4 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        double num3 = 4.5;
+        System.out.println(num2/num3);
+        System.out.println((float)num1/num2);
+    }
+}
