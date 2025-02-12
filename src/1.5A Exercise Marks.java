c402cdbd70321e43e99ce97e7abead612449daa2
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-12 14:22:12 -0500

    Commit

---
+++ src/1.5A Exercise Marks.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Exercise1_5A {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter your mark below:");
+        double mark = console.nextDouble();
+        if (mark >= 80) {
+            int level = 4;
+        } else if (mark ) {
+
+
+        }
+
+
+    }
+}
---
+++ src/1.5C Exercise Lottery.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Exercise1_5C {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter your mark below:");
+        double mark = console.nextDouble();
+        if (mark >= 80) {
+            int level = 4;
+        } else if () {
+
+
+        }
+
+
+    }
+}
