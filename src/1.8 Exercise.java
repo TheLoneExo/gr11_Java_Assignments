3afe1540d23824ef65da7fbbe7823980ee1884c4
TheLoneExo <shaungeomatrix@yahoo.com>
2025-02-24 14:00:12 -0500

    Commit

---
+++ src/1.8 Exercise.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class Exercise1_8 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i % 10 == 0){
+                System.out.println(i + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
+
+
+
