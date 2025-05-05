7a411d892650c833dfc1d12a32100ed0b49e7a92
TheLoneExo <shaungeomatrix@yahoo.com>
2025-05-04 23:12:27 -0400

    Commit

---
+++ src/Dog.java
@@ -0,0 +1,38 @@
+class Dog{
+
+
+    //instance variables
+    //#1 make variables for int age, String name, and String breed
+    int age;
+    String name;
+    String breed;
+
+    //constructor
+    public Dog(int age_, String name_, String breed_){
+        //#2 assign each parameter to each instance variable
+        age = age_;
+        name = name_;
+        breed = breed_;
+    }
+
+
+    //instance methods
+    //#3 write a method called bark that prints a barking sound like "ruff". There are no parameters.
+
+    public void bark() {
+        System.out.println("ruff");
+    }
+
+
+    //#4 write a method called toString that returns a String describing the dog (basically, return a String with the dog's instance variables). There are no parameters.
+    public String toString(){
+        if (age < 10) {
+            return "This puppy is " + age + " years old. It's name is " + name + ", and it is the " + breed + " breed.";
+        }
+        else {
+            return "This dog is " + age + " years old. It's name is " + name + ", and it is the " + breed + " breed.";
+        }
+
+    }
+
+}
---
+++ src/Exercise3_1.java
@@ -0,0 +1,15 @@
+class Exercise3_1 {
+    //instance variables
+    public static void main(String[] args) {
+        Dog coby = new Dog(11,"Coby", "Retriever");
+        System.out.println(coby);
+
+    }
+} 
+
+
+
+
+
+
+
